07732bf1631a1fd26c09f620c4f57bbdf11ce5c4
Juan V <jventura00@yahoo.com>
2022-02-07 13:33:27 -0600

    Adds basic layout and style to markdown and posts of the blog

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,7 +1,17 @@
 <x-guest-layout title="{{ $post->title }}">
-    {{ $post->date->diffForHumans() }}
-    @foreach ($post->categories as $category)
-        <a href="{{ route('page.category', $category) }}">{{ $category }}</a>    
-    @endforeach
-    {{ $post->contents }}
+    <div class="flex flex-col-reverse md:flex-row max-w-screen-lg mx-auto px-2 sm:px-6 lg:px-8">
+        <article class="prose md:pt-2 sm:px-6 lg:px-8 md:prose-lg lg:prose-xl sm:flex-col-reverse">
+            {{ $post->contents }}
+        </article>
+        <aside class="text-sm md:text-base lg:text-lg sm:px-6 lg-px-8 pt-2">
+            <div>
+                {{ $post->date->monthName . ' ' . $post->date->year }}
+            </div>
+            <div>
+                @foreach ($post->categories as $category)
+                    <a href="{{ route('page.category', $category) }}">{{ $category }}</a>    
+                @endforeach
+            </div>
+        </aside>
+    </div>
 </x-guest-layout>
