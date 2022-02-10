ab295d2376d957b5ed1e86955b21c6cec8620c28
Juan V <jventura00@yahoo.com>
2022-02-10 07:23:31 -0600

    Removes PageYearlyTest.php due to a typo error

--- tests/Feature/PageYearlyTest.php
+++ tests/Feature/PageYearlyTest.php
@@ -2,12 +2,36 @@
 
 namespace Tests\Feature;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Storage;
+use function Pest\Laravel\get;
+use Tests\Factories\PostFactory;
 
 beforeEach(function () {
     Storage::fake('projects');
 });
 
 it('shows a list of all the posts in the year', function () {
+    $lastYear = Carbon::today()->subYear(1);
 
+    $outdatedPost = PostFactory::new()
+        ->title('My mechanics')
+        ->date($lastYear)
+        ->content('This is a mechanics blog')
+        ->categories(['Mechanic', 'Logger'])
+        ->create();
+
+    $post = PostFactory::new()
+        ->title('Hello World')
+        ->content('My blog content')
+        ->categories(['Business', 'Laravel'])
+        ->create();
+
+    $today = Carbon::today();
+    $pathDate = $today->format('Y');
+
+    get("/blog/$pathDate")
+        ->assertStatus(200)
+        ->assertSee('Hello World')
+        ->assertDontSee('My mechanics');
 });
