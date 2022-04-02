f7e81d519be2c5563509ffcbbc3751335c54ed91
Juan C. Ventura <jventura00@gmail.com>
2022-04-01 21:20:49 -0600

    Fixes reload white bg

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,1 @@
 require('./bootstrap');
-
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
