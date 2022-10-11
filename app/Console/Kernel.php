70dede6d792271c98b1ca1b10bf04116b1649300
Juan V <jventura00@yahoo.com>
2022-10-11 06:44:40 -0600

    Names the cache dashboard data scheduled action

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->call(new CacheDashboardData())->everyTenMinutes();
+        $schedule->call(new CacheDashboardData())->everyTenMinutes()->name('cache_dashboard');
     }
 
     /**
